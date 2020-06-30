ae0749384ecd9d85a9df0dc5d1ff035fee8bcf5f
Casey Carroll <caseydean8@gmail.com>
2020-06-30 09:58:23 -0400

    Discover page gets data from Dog API

---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Pupster
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/about"
+          className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/discover"
+          className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+        >
+          Discover
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/search"
+          className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+        </Link>
+      </li>
+      {/* <li className="nav-item">
+        <Link
+          to="/contact/learn"
+          className={window.location.pathname === "/contact/learn" ? "nav-link active" : "nav-link"}
+        >
+          Learn
+        </Link>
+      </li> */}
+    </ul>
+  );
+}
+
+export default Nav;
